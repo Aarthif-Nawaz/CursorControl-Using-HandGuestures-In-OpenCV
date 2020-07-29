64cbef0fc32539880df28b311f38fa9b0e5528d9
Aarthif Nawaz <58284284+Aarthif-Nawaz@users.noreply.github.com>
2020-07-29 20:11:32 +0530

    Update HandGuestureControl.py

--- Cursor Control/HandGuestureControl.py
+++ Cursor Control/HandGuestureControl.py
@@ -16,13 +16,10 @@ while True:
     palm_cascade = palm.detectMultiScale(gray,1.3,5)
     for (x, y, w, h) in palm_cascade:
         cv2.rectangle(img, (x, y), ((x + w), (y + h)), (255, 0, 0), 2)
-        mouse.move(x, y,absolute=True,duration=.2);
+        mouse.move(x+w, y+h,absolute=True,duration=.2)
         #mouse.move(x, y)
 
-    fist_cascade = fist.detectMultiScale(gray, 1.3, 5)
-    for (xf, yf, wf, hf) in fist_cascade:
-        cv2.rectangle(img, (xf, yf), ((xf + wf), (yf + hf)), (0, 0, 255), 2)
-        mouse.move(xf, yf)
+
 
     # cv2.namedWindow("img",cv2.WND_PROP_FULLSCREEN)
     # cv2.setWindowProperty("img",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
