f9544f4b52859b051a85dcaee5d5a96fa40c9ac5
Aarthif Nawaz <58284284+Aarthif-Nawaz@users.noreply.github.com>
2020-07-31 16:59:17 +0530

    Update HandGuestureControl.py

--- Cursor Control/HandGuestureControl.py
+++ Cursor Control/HandGuestureControl.py
@@ -14,9 +14,17 @@ while True:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     palm_cascade = palm.detectMultiScale(gray,1.3,5)
+    hand = 0
     for (x, y, w, h) in palm_cascade:
         cv2.rectangle(img, (x, y), ((x + w), (y + h)), (255, 0, 0), 2)
-        mouse.move(x+w, y+h,absolute=True,duration=.2)
+        print(x,w,y,h)
+        # define range of skin color in HSV
+        lower_skin = np.array([0, 20, 70], dtype=np.uint8)
+        upper_skin = np.array([20, 255, 255], dtype=np.uint8)
+
+        # extract skin colur imagw
+
+        mouse.move(x+w, y+h,absolute=True)
         #mouse.move(x, y)
 
 
@@ -24,7 +32,7 @@ while True:
     # cv2.namedWindow("img",cv2.WND_PROP_FULLSCREEN)
     # cv2.setWindowProperty("img",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
     cv2.imshow('img', img)
-
+    cv2.imshow('hand', hand)
     k = cv2.waitKey(30) & 0xff
 
     if k == 27:
@@ -32,3 +40,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
